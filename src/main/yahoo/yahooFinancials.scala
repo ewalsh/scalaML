b32aacb31e62f90017d1681384894fed59578f1d
ewalsh <ewalsh@brandeis.edu>
2020-07-28 18:32:07 +0100

    up to step 4

--- src/main/yahoo/yahooFinancials.scala
+++ src/main/yahoo/yahooFinancials.scala
@@ -20,6 +20,47 @@ object YahooFinancials extends Enumeration {
     def load(filename: String): Try[Vector[DblPair]] = Try {
         val src = Source.fromFile(filename)
         val data = extract(src.getLines.map(_.split(",")).drop())
+        src.close
+        data
+    }
+
+    def extract(cols: Iterator[Fields]): ObsSet = {
+        val features = Array[YahooFinancials](LOW, HIGH, VOLUME)
+        val conversion = toArray(features)
+        cols.map(conversion(_)).toVector.map(x => Array[Double](1.0 - x(0)/x(1), x(2)))
+    }
+
+    class MinMax[T: ToDouble](val values: Vector[T]) {
+        val zero = (Double.MaxValue, -Double.MaxValue)
+        val (min, max) = values./:(zero){ case ((mn, mx), x) => {
+            val z = implicitly[ToDouble[T]].apply(x)
+            (if(z < mn) z else mn, if(z > mx) z else mx)
+        }}
+
+        case class ScaleFactors(
+            low: Double, high: Double, ratio: Double
+        )
+
+        var scaleFactors: Option[ScaleFactors] = None
+        def normalize(low: Double, high: Double): Vector[Double] = {
+            setScaleFactors(low, high).map(scale => {
+                values.map(x => {
+                    val z = implicitly[ToDouble[T]].apply(x)
+                    (z - min)*scale.ratio + scale.low
+                })
+            }).getOrElse(/* ... */)
+        }
+        def normalize(value: Double): Double = setScaleFactors.map(
+            scale =>
+                if(value < min) scale.low
+                else if (value > max) scale.high
+                else (value - min) * scale.high + scale.low
+        ).getOrElse(/* ... */)
+
+        def setScaleFactors(l: Double, h: Double): Option[ScaleFactors] = {
+            // .. error handling code
+            Some(ScaleFactors(l, h, (h - l)/(max - min)))
+        }
     }
 
 }
